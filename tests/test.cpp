b1b11c8916f418d7c9ca09a15c02b0e73e263795
ilyuzaaaaa <61176809+ilyuzaaaaa@users.noreply.github.com>
2020-12-06 06:54:05 +0500

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,4 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2020 Ilyuza Yangirova ilyuza.yangirova@gmail.com
 
 #include <gtest/gtest.h>
 
@@ -10,27 +10,21 @@ TEST(SharedPtr, Constructor) {
     auto ptr_int = new int(buff_int);
     auto ptr_string = new std::string(buff_string);
 
-    // Конструктор
     SharedPtr<int> shared_ptr_int(ptr_int);
     SharedPtr<std::string> shared_ptr_string(ptr_string);
     SharedPtr<int> shared_ptr_null;
 
-    // Оператор копирования
     auto shared_ptr_int_Copy = shared_ptr_int;
 
-    // Проверка
     EXPECT_EQ(shared_ptr_int_Copy.get(), ptr_int);
     EXPECT_EQ(shared_ptr_int_Copy.use_count(), 2);
 }
 TEST(SharedPtr, Move) {
-    // Конструктор
     SharedPtr<int> shared_ptr_int(new int(100));
     SharedPtr<std::string> shared_ptr_string(new std::string("tests"));
     SharedPtr<int> shared_ptr_null;
     SharedPtr<int> shared_ptr_int_Copy(shared_ptr_int);
-    // Конструктор (оператор) перемещения
     SharedPtr<int> shared_ptr_int_Move(std::move(shared_ptr_int_Copy));
-    // Проверка
     EXPECT_EQ(*shared_ptr_int_Move.get(), 100);
     EXPECT_EQ(shared_ptr_int_Move.use_count(), 2);
 }
@@ -40,11 +34,9 @@ TEST(SharedPtr, PointerToObj) {
     auto ptr_int = new int(buff_int);
     auto ptr_string = new std::string(buff_string);
 
-    // Конструктор
     SharedPtr<int> shared_ptr_int(ptr_int);
     SharedPtr<std::string> shared_ptr_string(ptr_string);
     SharedPtr<int> shared_ptr_null;
-    // Указывает ли указатель на объект
     EXPECT_EQ(static_cast<bool>(shared_ptr_int), true);
     EXPECT_EQ(static_cast<bool>(shared_ptr_string), true);
     EXPECT_EQ(static_cast<bool>(shared_ptr_null), false);
@@ -55,11 +47,9 @@ TEST(SharedPtr, CounterValue) {
     auto ptr_int = new int(buff_int);
     auto ptr_string = new std::string(buff_string);
 
-    // Конструктор
     SharedPtr<int> shared_ptr_int(ptr_int);
     SharedPtr<std::string> shared_ptr_string(ptr_string);
     SharedPtr<int> shared_ptr_null;
-    // Значение счётчика ссылок
     EXPECT_EQ(shared_ptr_int.use_count(), 1);
     EXPECT_EQ(shared_ptr_string.use_count(), 1);
     EXPECT_EQ(shared_ptr_null.use_count(), 0);
@@ -70,11 +60,9 @@ TEST(SharedPtr, ObjValue) {
     auto ptr_int = new int(buff_int);
     auto ptr_string = new std::string(buff_string);
 
-    // Конструктор
     SharedPtr<int> shared_ptr_int(ptr_int);
     SharedPtr<std::string> shared_ptr_string(ptr_string);
     SharedPtr<int> shared_ptr_null;
-    // Значение объекта (ссылка)
     EXPECT_EQ(*shared_ptr_int, buff_int);
     EXPECT_EQ(*shared_ptr_string, buff_string);
 }
@@ -84,37 +72,29 @@ TEST(SharedPtr, ObjPointer) {
     auto ptr_int = new int(buff_int);
     auto ptr_string = new std::string(buff_string);
 
-    // Конструктор
     SharedPtr<int> shared_ptr_int(ptr_int);
     SharedPtr<std::string> shared_ptr_string(ptr_string);
     SharedPtr<int> shared_ptr_null;
-    // Указатель на объект
     EXPECT_EQ(shared_ptr_int.get(), ptr_int);
     EXPECT_EQ(*shared_ptr_string.get(), "tests");
     EXPECT_EQ(shared_ptr_null.get(), nullptr);
 
-    // Указатель на объект
     EXPECT_EQ(shared_ptr_int.operator->(), ptr_int);
     EXPECT_EQ(shared_ptr_string->empty(), ptr_string->empty());
     EXPECT_EQ(shared_ptr_null.operator->(), nullptr);
 }
 TEST(SharedPtr, Methods) {
-    // Конструктор
     SharedPtr<int> shared_ptr_int(new int(100));
     SharedPtr<std::string> shared_ptr_string(new std::string("tests"));
     SharedPtr<int> shared_ptr_null;
-    // Свап
     shared_ptr_int.swap(shared_ptr_null);
 
-    // Проверка
     EXPECT_EQ(shared_ptr_int.get(), nullptr);
     EXPECT_EQ(*shared_ptr_null.get(), 100);
 
-    // Ресет
     shared_ptr_int.reset(new int(100));
     shared_ptr_null.reset();
 
-    // Проверка
     EXPECT_EQ(*shared_ptr_int.get(), 100);
     EXPECT_EQ(shared_ptr_null.get(), nullptr);
 }
